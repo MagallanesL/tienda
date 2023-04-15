a0db23f00faabe23ca9dee65cff8e573171feb10
Luis <alu.magallanes@gmail.com>
2023-04-15 13:25:06 -0300

    probando

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,21 @@
-from flask import Flask
+from flask import Flask , render_template
 
 app=Flask(__name__)
 
-def inicializar_app():
+
+
+app =Flask(__name__)
+app.debug= True
+
+@app.route('/')
+def index():
+    return  render_template('index.html')
+
+def pagina_no_encontrada(error):
+    return render_template('errores/404.html'),404
+
+
+def inicializar_app(Config):
+    app.config.from_object(Config)
+    app.register_error_handler(404, pagina_no_encontrada) #manejador de errores en el caso que se busque una pagina que no existe. en este caso un 404
     return app
